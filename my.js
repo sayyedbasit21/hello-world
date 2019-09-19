f85ffd34faa97c8f786289134a0ab0e4199bda0e
sayyedbasit21 <55497938+sayyedbasit21@users.noreply.github.com>
2019-09-19 19:55:40 +0530

    Update my.js

--- my.js
+++ my.js
@@ -1,11 +1,1 @@
-alert(Welcome to DEVELOPER ARENA);
 
-function hd(){
-let clickBt = document.getElementById("im");
- if(clickBt.style.display === "none"){
- clickBt.style.display = "block"
- }
-  else{
-  clickBt.style.display = "none"
-  }
-}
