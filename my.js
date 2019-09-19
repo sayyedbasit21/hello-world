2cac619f7ddfd346a9398e181200cf1ca6bde7be
sayyedbasit21 <55497938+sayyedbasit21@users.noreply.github.com>
2019-09-19 19:52:06 +0530

    Update my.js

--- my.js
+++ my.js
@@ -1,1 +1,9 @@
-
+function hd(){
+let click = document.getElementById("im");
+ if(click.style.display === "none"){
+ click.style.display = "block"
+ }
+  else{
+  click.style.display = "none"
+  }
+}
