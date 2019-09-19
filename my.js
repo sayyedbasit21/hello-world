0a1854beb733e07fb99fa58efe0f95daae8a3e18
sayyedbasit21 <55497938+sayyedbasit21@users.noreply.github.com>
2019-09-19 22:59:47 +0530

    Update my.js

--- my.js
+++ my.js
@@ -1,1 +1,12 @@
+alert("Welcome to DEVELOPER ARENA");
 
+function hd(){
+let clickBt = document.getElementById("octocat");
+ if(clickBt.style.display === "none"){
+ clickBt.style.display = "block"
+ }
+  else{
+  clickBt.style.display = "none"
+  }
+}
+  
